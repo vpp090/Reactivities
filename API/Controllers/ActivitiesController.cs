d31b0214f12d8df6179e11647868e0909f5a9d2a
vpp090 <vasil.petrov090@gmail.com>
2021-02-14 15:59:49 +0200

    CRUD activity operations completed

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -23,5 +23,24 @@ namespace API.Controllers
         {
             return await Mediator.Send(new Details.Query{ Id = id});
         }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+            return Ok(await Mediator.Send(new Create.Command{ Activity = activity }));
+        } 
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActiviy(Guid Id, Activity activity)
+        {
+            activity.Id = Id;
+            return Ok(await Mediator.Send(new Edit.Command{Activity = activity}));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActiviy(Guid id)
+        {
+            return Ok(await Mediator.Send(new Delete.Command{ Id = id }));
+        }
     }
 }
